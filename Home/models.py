e51a4dfab11ce5fc730db89c1e770feffc9c0dd4
Nicolas Sanguinetti <nico.sangui@hotmail.com>
2022-10-13 12:28:15 -0300

    Cree el models

--- Home/models.py
+++ Home/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class Persona(models.Model):
-    nombre = ...
-    apellido = ...
-    nacionalidad = ...
-    lugar_de_nacimeinto = ...
-    fecha_de_nacimiento = ...
     
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    nacionalidad = models.CharField(max_length=20)
+    lugar_de_nacimiento = models.CharField(max_length=20)
+    fecha_de_nacimiento = models.DateField()
+    edad = models.IntegerField()
+    equipo_actual = models.CharField(max_length=20)
+    posicion = models.CharField(max_length=20)
+    altura = models.DecimalField(decimal_places=2, max_digits=3)
+    peso = models.DecimalField(decimal_places=2, max_digits=5)
